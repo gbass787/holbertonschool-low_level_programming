30683ea6772b007fc41a3d19d60c9495668a65f8
gbass787 <4551@holbertonschool.com>
2022-06-06 16:52:26 +0000

    task1-4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ * main - entry point
+ * prints exactly "Programming is like building a multilingual puzzle
+ * Return - always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+		return (0);
+}

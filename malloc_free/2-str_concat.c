19f3c213939084a7d169438f24c7c641831e064c
gbass787 <4551@holbertonschool.com>
2022-06-25 22:29:26 +0000

    task2fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,6 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _strlen - returns str length
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	if (!*s)
+		return (0);
+	while (*s)
+	{
+		counter++;
+		s++;
+	}
+	return (counter);
+}
+
+/**
  * str_concat - concatenates two strings
  *
  * @s1: string 1
@@ -12,24 +34,29 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int x, y, size;
-	char *c;
+	char *new;
+	unsigned int x;
+	unsigned int y;
+	int total = 0;
 
-	for (x = 0; s1[x] != '\0'; x++)
-		;
-	for (y = 0; s2[y] != '\0'; y++)
-		;
-	size = x + y + 1;
-	c = malloc(sizeof(char) * size);
-	if (c == 0)
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	total += _strlen(s1) + _strlen(s2);
+	new = malloc((total + sizeof(char)) + 1);
+	if (new == NULL)
+	{
 		return (NULL);
-	for (x = 0; s1[x] != '\0'; x++)
-	{
-		*(c + x) = *(s1 + x);
 	}
-	for (y = 0; s2[y] != '\0'; y++)
+	for (x = 0; s1[x]; x++)
 	{
-		*(c + x + y) = *(s2 + y);
+		new[x] = s1[x];
 	}
-	return (c);
+	for (y = 0; s2[y]; y++, x++)
+	{
+		new[x] = s2[y];
+	}
+	new[x] = '\0';
+	return (new);
 }

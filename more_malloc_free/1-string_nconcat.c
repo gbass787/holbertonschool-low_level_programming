75b5c4a5a90e3473607c2a105dcd4f11d5527eb2
gbass787 <4551@holbertonschool.com>
2022-06-28 02:09:06 +0000

    task1fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,31 +12,44 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
+	unsigned int count, count2, x, y, sum = 0;
+	char *s;
 
-	if (s1 == NULL)
+	if (s1 == 0)
 		s1 = "";
-
-	if (s2 == NULL)
+	if (s2 == 0)
 		s2 = "";
 
-	for (index = 0; s1[index]; index++)
-		len++;
-	concat = malloc(sizeof(char) * (len + 1));
+	for (count = 0; s1[count] != '\0'; count++)
+		;
+	for (count2 = 0; s2[count2] != '\0'; count2++)
+		;
 
-	if (concat == NULL)
+	if (n >= count2)
+		sum = count + count2;
+	else
+		sum = count + n;
+	s = malloc(sizeof(char) * (sum + 1));
+
+	if (s == 0)
 		return (NULL);
 
-	len = 0;
-
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
-
-	return (concat);
+	for (x = 0; s1[x] != '\0'; x++)
+	{
+		s[x] = s1[x];
+	}
+	if (n < count2)
+		for (y = 0; y < n; y++)
+		{
+			s[x] = s2[y];
+			x++;
+		}
+	else
+		for (y = 0; s2[y] != '\0'; y++)
+		{
+			s[x] = s2[y];
+			x++;
+		}
+	s[x] = '\0';
+	return (s);
 }

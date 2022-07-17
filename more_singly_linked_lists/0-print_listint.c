ae33721827c9866e0ba0a9b4513739e1a3f7c2a3
gbass787 <4551@holbertonschool.com>
2022-07-17 08:00:00 +0000

    task0,1

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,21 @@
-123
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t list
+ *
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,20 @@
-123123123123123123123123123123123123
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ *
+ * @h: head
+ *
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int node = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node++;
+	}
+	return (node);
+}

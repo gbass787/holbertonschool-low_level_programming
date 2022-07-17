3b3aaca03d0a2aeac62e81c9b4a9c868366683e6
gbass787 <4551@holbertonschool.com>
2022-07-17 07:23:43 +0000

    singly linked lists,lists.h,task0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * print_list - prints allthe elements of a list_t list
+ * 
+ * @h: the linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}

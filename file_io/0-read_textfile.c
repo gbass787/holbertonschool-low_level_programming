75cf72d6e580dd1d41dc2650392bca5cb2c67f1f
gbass787 <4551@holbertonschool.com>
2022-07-21 06:13:46 +0000

    task0 fixed betty error

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-  * read_textfile - reads a text file and prints it to the POSIX standard output
+  * read_textfile - reads a text file and
+  * prints it to the POSIX standard output
   *
   * @filename: source file
   * @letters: number of letters to read and print

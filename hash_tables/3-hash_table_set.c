f1e9ff6e4d5c225cdf2b4481064d36e9773d3481
gbass787 <4551@holbertonschool.com>
2022-08-22 12:33:12 +0000

    fixing checker errors in task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,47 +1,43 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_set - function adds an element to the hash table
+ * hash_table_set - adds an element tot the hash table
  *
- * @ht: hash table you want to add or update the key/value to
- * @key: the key, cannot be an empty string
- * @value: value associated with key, value must be duplicated,
- * value can be an empty string
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with key
  *
- * Return: 1 on sucess, 0 on fail
- *
- * In case of collision, add new node at the beginning of the list
+ * Return: 1 if it succeeds, 0 if otherwise
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newNode = NULL;
-	char *key_copy, *value_copy;
+	char *dup_value;
 	unsigned long int index = 0;
+	hash_node_t *new;
 
-	if ((strcmp(key, "") == 0) || key == NULL || value == NULL || ht == NULL)
+	if (strcmp(key, "") == 0)
 		return (0);
 
-	key_copy = strdup(key);
-	value_copy = strdup(value);
+	dup_value = strdup(value);
+	if (dup_value == NULL)
+		return (0);
 
-	newNode = malloc(sizeof(hash_node_t));
-		if (newNode == NULL)
-			return (0);
-	newNode->key = key_copy;
-	newNode->value = value_copy;
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
+
+	new->key = (char *)key;
+	new->value = dup_value;
 
 	if (ht->array[index] == NULL)
-		newNode->next = NULL;
+		new->next = NULL;
 	else
-		newNode->next = ht->array[index];
+		new->next = ht->array[index];
 
-	ht->array[index] = newNode;
+	ht->array[index] = new;
 
 	return (1);
 }

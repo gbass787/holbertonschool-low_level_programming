d28e4552e5678f432371d5061cda63b95b9fb2d7
gbass787 <4551@holbertonschool.com>
2022-08-22 12:29:15 +0000

    trying to fix checker errors

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,31 +1,45 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - deletes a hash table
+ * free_link_list - free linked list in hash table
+ *
+ * @head: beginning of linked list
+ *
+ * Return: void
+ */
+
+void free_link_list(hash_node_t *head)
+{
+	hash_node_t *pop_candidate;
+	hash_node_t *change_node;
+
+	pop_candidate = head;
+	while (pop_candidate)
+	{
+		change_node = pop_candidate->next;
+		free(pop_candidate->value);
+		free(pop_candidate->key);
+		free(pop_candidate);
+		pop_candidate = change_node;
+	}
+}
+
+/**
+ * hash_table_delete - free hash table
  *
  * @ht: hash table
+ *
+ * Return: void
  */
 
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned int index = 0;
-	hash_node_t node_pointer;
+	size_t idx = 0;
 
-	while (ht->array[index])
+	while (idx < ht->size)
 	{
-		if (ht->array[index])
-		{
-			while (ht->array[index])
-			{
-				node_pointer = ht->array[index]->next;
-				free(ht->array[index]);
-				ht->array[index] = node_pointer;
-			}
-		}
-		else
-			free(ht->array[index]);
-
-		index++;
+		free_link_list(ht->array[idx]);
+		idx++;
 	}
 	free(ht->array);
 	free(ht);
